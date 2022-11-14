5b4fd34cf93924cca609f2660126d7864859b581
Charles Junior <selrahcjunior@hotmail.com>
2022-11-14 20:08:13 -0300

    Alterações de teste

---
+++ prisma/migrations/20221114220735_gettin_back/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `firstTeamPoints` on the `Guess` table. The data in that column could be lost. The data in that column will be cast from `String` to `Int`.
+  - You are about to alter the column `secondTeamPoints` on the `Guess` table. The data in that column could be lost. The data in that column will be cast from `String` to `Int`.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Guess" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "firstTeamPoints" INTEGER NOT NULL,
+    "secondTeamPoints" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "gameId" TEXT NOT NULL,
+    "partcipantId" TEXT NOT NULL,
+    CONSTRAINT "Guess_partcipantId_fkey" FOREIGN KEY ("partcipantId") REFERENCES "Participant" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Guess_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Guess" ("createdAt", "firstTeamPoints", "gameId", "id", "partcipantId", "secondTeamPoints") SELECT "createdAt", "firstTeamPoints", "gameId", "id", "partcipantId", "secondTeamPoints" FROM "Guess";
+DROP TABLE "Guess";
+ALTER TABLE "new_Guess" RENAME TO "Guess";
+CREATE UNIQUE INDEX "Guess_partcipantId_gameId_key" ON "Guess"("partcipantId", "gameId");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
